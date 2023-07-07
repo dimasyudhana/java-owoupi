d1107b18a1168b42d89eb3bbf8b3ab1191f7edc8
dimasyudhana <dimas.yudhana@gmail.com>
2023-07-07 21:59:23 +0800

    switch-lambda

--- SwitchStatement.java
+++ SwitchStatement.java
@@ -16,5 +16,37 @@ public class SwitchStatement {
             default:
                 System.out.println("salah jurusan mungkin bro");
         }
+
+
+        // switch-lambda sebagai opsi praktis up to java 14
+        switch (nilai) {
+            case "A" -> System.out.println("Wow mantab lulus bro");
+            case "B", "C" -> System.out.println("yaa lumayan bro");
+            case "D" -> System.out.println("coba lagi bro");
+            default -> System.out.println("salah jurusan mungkin bro");
+        }
+
+        String ucapan;
+        switch (nilai) {
+            case "A" -> ucapan = "Wow mantab lulus bro";
+            case "B", "C" -> ucapan = "yaa lumayan bro";
+            case "D" -> ucapan = "coba lagi bro";
+            default -> ucapan = "salah jurusan mungkin bro";
+        };
+
+        System.out.println(ucapan);
+
+        ucapan = switch (nilai) {
+            case "A":
+                yield "Wow mantab lulus bro";
+            case "B", "C":
+                yield "yaa lumayan bro";
+            case "D":
+                yield  "coba lagi bro";
+            default :
+                yield "salah jurusan mungkin bro";
+        };
+
+        System.out.println(ucapan);
     }
 }
